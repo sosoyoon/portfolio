25a30427e5a6256705b4ef611f25e37d82f2d424
sosoyoon <soyoon36@gmail.com>
2022-07-17 22:32:25 +0900

    edit file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,12 @@
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 250,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".btn-nav",
-      clickable: true,
-    }
-  });
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
