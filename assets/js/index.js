32fc5009ca44ba221574b0783624d70273dfadb8
sosoyoon <soyoon36@gmail.com>
2022-08-22 10:41:59 +0900

    edit file

---
+++ assets/js/index.js
@@ -0,0 +1,120 @@
+visualTl = gsap.timeline({
+    defaults:{
+      duration:0.5,
+    }
+  })
+  visualTl.addLabel('label1')
+  .from('.sc_visual .sc_title' ,{y:100, opacity:0, duration: 1.5}, 'label1+=0.5')
+  .from('.sc_visual .sc_sub' ,{y:100, opacity:0, duration: 1.5}, 'label1+=0.7')
+  .from('.sc_visual .inner01 .text_box .sc_desc' ,{y:100, opacity:0, duration: 1.5}, 'label1+=0.9')
+  .from('.m2_box' ,{y:100, opacity:0, duration: 1.5}, 'label1+=1.1')
+  .from('.price_box' ,{y:100, opacity:0, duration: 1.5}, 'label1+=1.3')
+  
+
+
+$(function(){
+    const firstBar = gsap.timeline({})
+    firstBar.addLabel('a')
+    .from('#m1_tab1 .bar_m2',{width:'0%'},'a')
+    .from('#m1_tab1 .bar_m1',{width:'0%'},'a')
+    ScrollTrigger.create({
+        trigger:".inner02",
+        start:"0 45%",
+        end:"bottom 0",
+        // markers:true,
+        animation:firstBar
+    })
+    const secBar = gsap.timeline({})
+    secBar.addLabel('a')
+    .from('#intel_tab1 .bar_m2',{width:'0%'},'a')
+    .from('#intel_tab1 .bar_m1',{width:'0%'},'a')
+    ScrollTrigger.create({
+        trigger:".graph_box",
+        start:"top 2%",
+        end:"bottom 0",
+        // markers:true,
+        animation:secBar
+    })
+    
+
+
+    $('.btn_graph').click(function(e){
+        e.preventDefault();
+        let event = $(this).data('target')
+        
+        $(this).addClass('active').siblings().removeClass('active');
+        $(event).addClass('active').siblings().removeClass('active');
+       
+        
+        m2 = $(event).find('.bar_m2');
+        m1 = $(event).find('.bar_m1');
+        gsap.from(m2,{width:'0%'})
+        gsap.from(m1,{width:'0%'})
+    })
+
+    textAni = gsap.timeline({
+        scrollTrigger:{
+            trigger:".secu",
+            start:"top 40%",
+            end:"bottom 100%",
+            duration: 5,
+            // markers:true,
+            scrub:1,
+        }
+    })
+    textAni.addLabel('a')
+    .to('.flex_bottom .flex_70 .dot',{ display:'none', stagger:3 },'a')
+    .to('.flex_bottom .flex_70 .text',{ display:'block', stagger:3 },'a+=0.5')
+
+    let lastScroll = 0;
+    $(window).scroll(function(){
+        curScroll = $(this).scrollTop();
+        target = $('.header_center').offset().top;
+        if(curScroll > lastScroll){
+            $('.header_center').addClass('show').removeClass('hide')
+        }else if(curScroll == lastScroll){
+            $('.header_center').removeClass('show').removeClass('hide')
+        }
+        else{
+            $('.header_center').addClass('hide').removeClass('show')
+        }
+    })
+
+    $('[data-fade]').each(function(i,e){ 
+        gsap.from(e, 1.2,{
+            scrollTrigger:{
+              trigger:e,
+              start:"top 90%", 
+              end:"bottom top", 
+            },
+            opacity: 0.2,
+            y:0,
+            markers:true,
+          })
+      })
+    
+      $('.color_box button').click(function(e){
+        e.preventDefault();
+        
+        $(this).parent().addClass('active').siblings().removeClass('active');
+    })
+    
+    // var fillup = gsap.timeline({})
+    // fillup.addLabel('a')
+    // // .from('.battery .flex_30 .sub',{top:'100%'},'a').to('.battery .flex_30 .sub',{top:'0%'},{background: '#4cd265'},'a')
+    // // .from('.battery .flex_30 .sub',{background: '#4cd265'},'a')
+    // ScrollTrigger.create({
+    //     trigger:".battery",
+    //     start:"top 20%",
+    //     end:"bottom 0",
+    //     markers:true,
+    //     animation:fillup,
+    // })
+
+
+    // TweenLite.to(".battery .flex_30 .sub", 1, {background:"#4cd265", delay:0.5})
+
+    // $('.battery .flex_30 .sub').css({background: "#4cd265", transition: "2.0s"});
+    
+    // each문 써서 페이드 쓰기
+})
