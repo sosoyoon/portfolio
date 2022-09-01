0cdd0ee67a6ec8092d108fcb27c64000433f89ce
sosoyoon <soyoon36@gmail.com>
2022-09-01 14:54:52 +0900

    edit file

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,16 +10,6 @@ var swiper = new Swiper(".sc_architectuur .swiper", {
 });
 
 
-// gsap.to() 후
-// gsap.form() 전
-// gsap.fromTo() 전후
-
-// gsap.from('.intro01' ,{x:-100})
-// gsap.from('.intro02' ,{x:-100})
-// gsap.from('.intro03' ,{y:100})
-// gsap.from('.intro04' ,{x:-100})
-// gsap.from('.intro05' ,{x:-100})
-
 introTl = gsap.timeline({
   defaults:{
     duration:0.5,
@@ -34,27 +24,12 @@ introTl.addLabel('label1')
 .from('.intro05' ,{x:-100, opacity:0}, 'label1+=0.4')
 .from('.sc_intro .title' ,{scale:0.9, opacity: 0}, 'label1+=0.4')
 
-// $('[data-fade]').length
-
-// gsap.from('.sc_intro .desc_box', 0.6,{
-
-//   scrollTrigger:{
-//     trigger:".sc_intro .desc_box",
-//     start:"top 80%", //[트리거기준 윈도우기준]
-//     end:"bottom top", //[트리거기준 윈도우기준]
-//     markers:true,
-//   },
-
-//   opacity: 0,
-//   y:100,
-// })
-
-$('[data-fade]').each(function(i,e){ // i = > 인덱스, e = > 엘리먼트
+$('[data-fade]').each(function(i,e){
   gsap.from(e, 0.6,{
       scrollTrigger:{
         trigger:e,
-        start:"top 80%", //[트리거기준 윈도우기준]
-        end:"bottom top", //[트리거기준 윈도우기준]
+        start:"top 80%", 
+        end:"bottom top", 
       },
     
       opacity: 0,
@@ -62,12 +37,12 @@ $('[data-fade]').each(function(i,e){ // i = > 인덱스, e = > 엘리먼트
     })
 })
 
-$('[data-scale]').each(function(i,e){ // i = > 인덱스, e = > 엘리먼트
+$('[data-scale]').each(function(i,e){ 
   gsap.from(e, 0.8,{
       scrollTrigger:{
         trigger:e,
-        start:"top 80%", //[트리거기준 윈도우기준]
-        end:"bottom top", //[트리거기준 윈도우기준]
+        start:"top 80%", 
+        end:"bottom top", 
       },
     
       opacity: 0,
